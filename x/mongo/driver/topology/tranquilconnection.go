0ad5de72e59f7f8026b7813f2bc8d999645dc2d0
Trek Palmer <tsp@tranquildata.com>
2020-06-18 14:48:58 -0400

    Need to peel back the layers of the execute onion

--- x/mongo/driver/topology/tranquilconnection.go
+++ x/mongo/driver/topology/tranquilconnection.go
@@ -2,7 +2,6 @@ package topology
 
 import (
 	"context"
-	"errors"
 	"net"
 	"sync/atomic"
 
@@ -38,17 +37,7 @@ func (tqConn *Tranquilconn) WriteWireMessage(ctx context.Context, msg []byte) er
 }
 
 func (tqConn *Tranquilconn) ReadWireMessage(ctx context.Context, dst []byte) (hdr *wiremessage.MsgHeader, wm []byte, err error) {
-	wm, err = tqConn.mongoconn.ReadWireMessage(ctx, dst)
-	if err != nil {
-		return nil, nil, err
-	}
-	var ok bool
-	hdr = &wiremessage.MsgHeader{}
-	hdr.Length, hdr.RequestID, hdr.ResponseTo, hdr.Opcode, _, ok = wiremessage.ReadHeader(wm)
-	if !ok {
-		return nil, nil, errors.New("Incomplete header")
-	}
-	return
+	return driver.ReadWireMessageFromConn(ctx, tqConn.mongoconn, dst)
 }
 
 func (tqConn *Tranquilconn) Close() {
